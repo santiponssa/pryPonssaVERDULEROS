a0b0c6825f716a97785368528742ba4014486147
root <mgiraudo@ies21.edu.ar>
2023-06-07 13:13:05 -0300

    fdsfsd

--- FrmLogin.Designer.cs
+++ FrmLogin.Designer.cs
@@ -136,6 +136,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ingreso";
             this.Load += new System.EventHandler(this.FrmLogin_Load);
             this.groupBox1.ResumeLayout(false);
